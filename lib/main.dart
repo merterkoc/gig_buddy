831fa0d413f966c9acf27bfdd8ba7982fb8617da
mert <mertyasarerkocc@gmail.com>
2025-07-18 01:04:32 +0300

    Splash configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:gig_buddy/app.dart';
 import 'package:gig_buddy/app_initialize.dart';
 import 'package:gig_buddy/src/route/router.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await AppInitializationService.initialize();
+  FlutterNativeSplash.remove();
   return runApp(
     GigBuddyApp(
       settingsController: settingsController,
